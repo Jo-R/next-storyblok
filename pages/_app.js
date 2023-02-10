5c5fb5f0cb68c3360ae124b9b90491e1d1e1bbe0
Jo Rigg <jo.rigg@earthware.co.uk>
2023-02-10 15:47:32 +0000

    initial commit from tutorial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: "uc1QVkdiO8m1JxGEA8sPcwtt", // TODO in .env
+  use: [apiPlugin],
+  components,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
